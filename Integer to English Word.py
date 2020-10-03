2a3fd011063ef160d000c87076278fe1b3c6aeb7
Saptarshi Neogi <neogisaptarshi@gmail.com>
2020-10-03 21:28:18 +0530

    Optimized the program.

--- Integer to English Word.py
+++ Integer to English Word.py
@@ -25,25 +25,24 @@ def number_to_english(n):
         return TENS[n]
     elif n < 100:
         return number_to_english(n - (n % 10)) + ' ' + number_to_english(n % 10)
-    elif n < 1000 and n % 100 == 0:
+    elif n < 10**3 and n % 100 == 0:
         return number_to_english(int(n/100)) + ' Hundred'
-    elif n < 1000:
+    elif n < 10**3:
         return number_to_english(int(n/100)) + ' Hundred ' + number_to_english(n % 100)
-    elif n<1000000 and n>=1000 and n%1000==0:
+    elif n<10**6 and n>=1000 and n%1000==0:
         return number_to_english(int(n/1000)) + ' Thousand'
-    elif n < 1000000:
-        return number_to_english(int(n/1000)) + ' Thousand ' + number_to_english(n % 1000)
-    elif n<1000000000 and n>=1000000 and n%1000000==0:
-        return number_to_english(int(n/1000000)) + ' Million'
-    elif n<1000000000 and n>=1000000:
-        return number_to_english(int(n/1000000)) + ' Million ' + number_to_english(n%1000000)
-    elif n<2**31-1 and n>=1000000000 and n%1000000000==0:
-        return number_to_english(int(n/1000000000)) + ' Billion'
-    elif n<=2**31-1 and n>=1000000000:
-        return number_to_english(int(n/1000000000)) + ' Billion ' + number_to_english(n%1000000000)
+    elif n < 10**6:
+        return number_to_english(int(n/1000)) + ' Thousand ' + number_to_english(n % 10**3)
+    elif n<10**9 and n>=10**6 and n%10**6==0:
+        return number_to_english(int(n/10**6)) + ' Million'
+    elif n<10**9 and n>=10**6:
+        return number_to_english(int(n/10**6)) + ' Million ' + number_to_english(n%10**6)
+    elif n<2**31-1 and n>=10**9 and n%10**9==0:
+        return number_to_english(int(n/10**9)) + ' Billion'
+    elif n<=2**31-1 and n>=10**9:
+        return number_to_english(int(n/10**9)) + ' Billion ' + number_to_english(n%10**9)
     
     return ''
-
 
 
 print(number_to_english(num))
