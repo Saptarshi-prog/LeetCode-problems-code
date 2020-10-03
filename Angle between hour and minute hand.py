4fd06a1867f62fd364937910f5d6af4a246257e5
Saptarshi Neogi <neogisaptarshi@gmail.com>
2020-10-03 21:35:29 +0530

    Update Angle between hour and minute hand.py

--- Angle between hour and minute hand.py
+++ Angle between hour and minute hand.py
@@ -1,18 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep  9 23:06:38 2020
-
-@author: 91842
-"""
 hour,minutes = [int(i) for i in input().split()]
-
-a=hour+minutes/60
-b = minutes/5
-
-c = max(a,b)
-d = min(a,b)
-        
-ans = (c-d)*30
-ans1 = 360-ans
-ans2 = min(ans,ans1)
-print(ans2)
+hour=hour+minutes/60
+minutes = minutes/5
+ans = (max(hour,minutes)-min(hour,minutes))*30
+print(min(ans,360-ans))
